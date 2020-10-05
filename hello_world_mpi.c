66d4c75cc94eba958afda4774fd9e6671a101989
Simon Kern <skern@mech.kth.se>
2020-10-05 15:43:03 +0200

    corrected typos

--- hello_world_mpi.c
+++ hello_world_mpi.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]){
    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
    MPI_Comm_size(MPI_COMM_WORLD,&size);
 
-   printf("Hello World form rank %d form %d processes!\n",rank,size);
+   printf("Hello World from rank %d from %d processes!\n",rank,size);
 
    MPI_Finalize();
 }
