52098cac759fa599862f47436fb5a21b5bfcda6a
Simon Kern <skern@mech.kth.se>
2020-10-06 13:43:39 +0200

    Added time(NULL) to srand call.

--- pi_mpi.c
+++ pi_mpi.c
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     
-    srand(123456789 + rank*SEED); // Important: Multiply SEED by "rank" when you introduce MPI!
+    srand(time(NULL) + 123456789 + rank*SEED); // Important: Multiply SEED by "rank" when you introduce MPI!
 
     // Split the work !
     if (rank == 0) {   
